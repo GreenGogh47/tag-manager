e68e1f3b016ee3fca696d1302b2ac2fda4b8c1e1
Chris Crane <chrisjcrane@gmail.com>
2023-09-25 14:25:30 -0600

    space facade, poro, and passing test

--- app/facades/spaces_facade.rb
+++ app/facades/spaces_facade.rb
@@ -1,7 +1,18 @@
 class SpacesFacade
-  service = SpacesService.new.get_spaces(ENV["MY_TEAM_ID"])
+attr_reader :spaces
 
-  service[:spaces].each do |space|
-    Spaces.new(space)
+  def initialize
+    @spaces = []
+    load_spaces
+  end
+
+  private
+  
+  def load_spaces
+    service = SpacesService.new.get_spaces(ENV["MY_TEAM_ID"])
+
+    service[:spaces].each do |space_data|
+      @spaces << Space.new(space_data) # Create Space objects and add them to the array
+    end
   end
 end
