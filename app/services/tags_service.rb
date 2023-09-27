0676b1d7698dab0f918a169f49adff47613fad1a
Chris Crane <chrisjcrane@gmail.com>
2023-09-27 09:00:50 -0600

    abstract initial figaro connection into 'base_service'

--- app/services/tags_service.rb
+++ app/services/tags_service.rb
@@ -1,16 +1,5 @@
-class TagsService
+class TagsService < BaseService
   def get_tags(space_id)
     get_url("space/#{space_id}/tag")
-  end
-
-  def get_url(url)
-    response = connection.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-  
-  def connection
-    Faraday.new(url: "https://api.clickup.com/api/v2/") do |f|
-      f.headers["Authorization"] = ENV["CLICKUP_API_KEY"]
-    end
   end
 end
