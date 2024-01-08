3176b7e61f22ecd39d287c05144304a8c602fd48
Chris Crane <chrisjcrane@gmail.com>
2024-01-08 14:23:05 -0700

    refactor team service and add spec file

--- app/services/team_service.rb
+++ app/services/team_service.rb
@@ -1,11 +1,28 @@
+# class TeamService < BaseService
+#   def get_teams
+#     get_url("team")
+#   end
+
+#   def get_my_team
+#     data = get_url("team")
+#     my_team_id = data[:teams].first[:id]
+#     my_team_id
+#   end
+# end
+
 class TeamService < BaseService
   def get_teams
     get_url("team")
   end
 
-  def get_my_team
+  def my_team_id
+    @my_team_id ||= fetch_my_team_id
+  end
+
+  private
+
+  def fetch_my_team_id
     data = get_url("team")
-    my_team_id = data[:teams].first[:id]
-    my_team_id
+    data[:teams].first[:id]
   end
 end
