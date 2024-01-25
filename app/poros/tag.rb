124c09c3927d3dfae26e43e4f417c46e58f8b296
Chris Crane <chrisjcrane@gmail.com>
2024-01-25 09:53:02 -0700

    add dropdown for tags and popper

--- app/poros/tag.rb
+++ app/poros/tag.rb
@@ -1,13 +1,13 @@
 class Tag
   attr_reader :name,
-              :project_id,
+              :space_id,
               :tag_fg,
               :tag_bg,
               :creator
 
   def initialize(data)
     @name = data[:name]
-    @project_id = data[:project_id]
+    @space_id = data[:project_id]
     @tag_fg = data[:tag_fg]
     @tag_bg = data[:tag_bg]
     @creator = data[:creator]
