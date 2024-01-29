d1858963aa4dda2fc02b1e8675ca4212cc0e4ffc
Chris Crane <chrisjcrane@gmail.com>
2024-01-28 20:34:41 -0700

    add tag validations and spec.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:space_id) }
+    it { should validate_presence_of(:tag_fg) }
+    it { should validate_presence_of(:tag_bg) }
+    it { should validate_presence_of(:creator) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:space) }
+  end
 end
