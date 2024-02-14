61ea1905cb8f0a06ca26cc5b0635ec18d3cc86c9
Chris Crane <chrisjcrane@gmail.com>
2024-02-14 14:33:39 -0700

    update global tags on index view.

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,6 +4,7 @@ class SpacesController < ApplicationController
     my_team = service.my_team_id
     @spaces_facade = SpacesFacade.new(my_team, service)
     @spaces = Space.all
-    @global_tags = Space.global_tags
+    @global_tags = Space.global_tags.uniq { |tag| tag.name }
+    # require 'pry'; binding.pry
   end
 end
