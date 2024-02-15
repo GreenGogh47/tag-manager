8189c5393da29072bb85f3146d536028160f96e4
Chris Crane <chrisjcrane@gmail.com>
2024-02-14 17:45:14 -0700

    add tag show page and update with put request to clickup api

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,12 @@
 class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  
   def update
-    tag = Tag.find(params[:id])
-
-    if tag.update(tag_params)
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      clickup_service.update_tag(@tag.space_id, @tag.name, tag_params)
       redirect_to spaces_path, notice: "Tag Updated!"
     else
       redirect_to spaces_path, notice: "sumins rong"
@@ -13,5 +17,9 @@ class TagsController < ApplicationController
 
   def tag_params
     params.require(:tag).permit(:name, :tag_fg, :tag_bg)
+  end
+
+  def clickup_service
+    @clickup_service ||= ClickupApiService.new
   end
 end
--- app/services/clickup_api_service.rb
+++ app/services/clickup_api_service.rb
@@ -20,14 +20,24 @@ class ClickupApiService
     @my_team_id ||= fetch_my_team_id
   end
 
-  # Tags and Spaces
+  # Fetch Tags and Spaces
   def get_tags(space_id)
     get_url("space/#{space_id}/tag")
   end
 
   def get_spaces(team_id)
-    x = get_url("team/#{team_id}/space")
+    get_url("team/#{team_id}/space")
+  end
 
+  # CRUD
+
+  def update_tag(space_id, tag_name, tag_params)
+    response = connection.put("space/#{space_id}/tag/#{tag_name}") do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { tag: tag_params }.to_json
+    end
+
+    handle_response(response)
   end
 
   private
@@ -35,5 +45,21 @@ class ClickupApiService
   def fetch_my_team_id
     data = get_url("team")
     data[:teams].first[:id]
+  end
+
+  def handle_response(response)
+    case response.status
+    when 200...300
+      JSON.parse(response.body, symbolize_names: true)
+    when 400...600
+      handle_error(response)
+    else
+      raise "Unexpected response status: #{response.status}"
+    end
+  end
+
+  def handle_error(response)
+    error_message = JSON.parse(response.body)["error"]
+    raise "API error: #{error_message}"
   end
 end
